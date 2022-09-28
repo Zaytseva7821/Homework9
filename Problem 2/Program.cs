cc10b20c169772da7a050c00e9d2b3f66c403701
Anastasia <zaytseva7821@ya.ru>
2022-09-29 00:26:06 +0400

    problem 2

---
+++ Problem 2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int DataEntry(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumFromMToN(int M, int N, int sum)
+{
+    if (M>N)
+    {
+    System.Console.WriteLine(sum);
+    }
+    else
+    {
+    sum = sum + N;
+    SumFromMToN(M,N-1,sum);
+    }
+}
+
+int M = DataEntry("Задайте значение M");
+int N = DataEntry("Задайте значение N");
+if (N==M)
+{
+    System.Console.WriteLine("Заданные числа равны");
+}
+if (N<M)
+{
+    int temp = N;
+    N = M;
+    M = temp;
+}
+int sum = N-M;
+SumFromMToN(M, N, 0);
